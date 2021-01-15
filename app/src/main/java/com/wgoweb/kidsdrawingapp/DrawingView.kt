c939089d304a54d1f73b140804837c4b1afcbba0
Waleerat S <waleerat.gottlieb@gmail.com>
2021-01-15 12:52:14 +0100

    Initial commit

---
+++ app/src/main/java/com/wgoweb/kidsdrawingapp/DrawingView.kt
@@ -0,0 +1,105 @@
+package com.wgoweb.kidsdrawingapp
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.MotionEvent
+import android.view.View
+
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+    private var mDrawPath: CustomPath? = null
+    private var mCanvasBitMap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private  var mCanvasPaint: Paint? = null
+    private  var mBrushSize: Float = 0.toFloat()
+    private  var color = Color.BLACK
+    private var canvas: Canvas? = null
+
+    // make drawing stay
+    private  val mPaths = ArrayList<CustomPath>()
+
+    init {
+        setUpDrawingAtStart()
+    }
+
+    private fun setUpDrawingAtStart(){
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.SQUARE
+
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+
+    }
+    // If size has been change
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        mCanvasBitMap = Bitmap.createBitmap(h,w, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitMap!!)
+    }
+    // When wanted to draw
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitMap!!, 0f, 0f, mCanvasPaint)
+
+        for(path in mPaths) {
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.color
+            canvas.drawPath(path, mDrawPaint!!)
+        }
+
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when(event?.action){
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+
+                mDrawPath!!.reset()
+                if (touchX != null && touchY != null) {
+                        mDrawPath!!.moveTo(touchX, touchY)
+                }
+
+            }
+            MotionEvent.ACTION_MOVE -> {
+                if (touchX != null && touchY != null) {
+                    mDrawPath!!.lineTo(touchX, touchY)
+                }
+            }
+            MotionEvent.ACTION_UP -> {
+                mPaths.add(mDrawPath!!)
+                mDrawPath = CustomPath(color, mBrushSize)
+
+            }
+            else -> return  false
+        }
+        invalidate()
+        return true
+    }
+
+    fun setSizeForBrush(newSize: Float) {
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics)
+
+        mDrawPaint!!.strokeWidth = mBrushSize
+    }
+
+    internal  inner class CustomPath(var color: Int,
+                                     var brushThickness: Float
+                                     ): Path() {
+
+
+    }
+}
---
+++ app/src/main/java/com/wgoweb/kidsdrawingapp/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.wgoweb.kidsdrawingapp
+
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_brush_size.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var mImageButtonCurrentPaint: ImageButton? = null // A variable for current color is picked from color pallet.
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawing_view.setSizeForBrush(20.toFloat())
+        mImageButtonCurrentPaint = ll_paint_colors[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.pallet_pressed
+            )
+        )
+
+        ib_brush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+
+    }
+
+    
+
+    private fun showBrushSizeChooserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size :")
+        val smallBtn = brushDialog.ibSmallBrush
+        smallBtn.setOnClickListener(View.OnClickListener {
+            drawing_view.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        })
+        val mediumBtn = brushDialog.ibMediumBrush
+        mediumBtn.setOnClickListener(View.OnClickListener {
+            drawing_view.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        })
+
+        val largeBtn = brushDialog.ibLargeBrush
+        largeBtn.setOnClickListener(View.OnClickListener {
+            drawing_view.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        })
+        brushDialog.show()
+    }
+}
